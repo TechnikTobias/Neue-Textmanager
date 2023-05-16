6f5b60e248ebdaa0becd300415bf49fedcf90c40
TechnikTobias <tobias.giebelhaus@gmx.de>
2023-05-16 19:17:05 +0200

    main

--- Neue_Textmanager.py
+++ Neue_Textmanager.py
@@ -12,20 +12,22 @@ Programm_ort = os.getlogin()
 
 def Start():
     global Textmanager
+    Settings.Check_settings()
     Textmanager = Tk()
     Textmanager.title("Textmanager")
+    Textmanager.config(bg=Settings.Textmanager_Hintergrund)
     Textmanager.geometry("1040x800")
     Textmanager.minsize(width=1040, height=850)
     Textmanager.iconbitmap(f"C:\\Users\\{Programm_ort}\\Desktop\\Lieder\\picture_compress 1.ico")
-    Settings.Check_settings()
     Menu_generator()
+    Load_Setting()
 
 def Menu_generator():
-    Menu_Settings = Menu(Textmanager)
-    Menu_Info = Menu(Menu_Settings)
-    Menu_Kamera = Menu(Menu_Settings)
-    Menu_LiedKontrolle = Menu(Menu_Settings)
-    Menu_Help = Menu(Menu_Settings)
+    Menu_Settings = Menu(Textmanager, bg=Settings.Textmanager_Hintergrund, fg=Settings.Textmanager_Textfarbe, border=0)
+    Menu_Info = Menu(Menu_Settings, bg=Settings.Textmanager_Hintergrund, fg=Settings.Textmanager_Textfarbe, border=0)
+    Menu_Kamera = Menu(Menu_Settings, bg=Settings.Textmanager_Hintergrund, fg=Settings.Textmanager_Textfarbe, border=0)
+    Menu_LiedKontrolle = Menu(Menu_Settings, bg=Settings.Textmanager_Hintergrund, fg=Settings.Textmanager_Textfarbe, border=0)
+    Menu_Help = Menu(Menu_Settings, bg=Settings.Textmanager_Hintergrund, fg=Settings.Textmanager_Textfarbe, border=0, borderwidth=0)
     Menu_Settings.add_cascade(label="Info", menu= Menu_Info)
     Menu_Settings.add_cascade(label="Kamera", menu=Menu_Kamera)
     Menu_Settings.add_cascade(label="Lied Kontrolle", menu=Menu_LiedKontrolle)
@@ -42,7 +44,7 @@ def Menu_generator():
 def Load_Setting():
     if Settings.see_the_text:
         AnzeigeText = Toplevel(Textmanager)
-        AnzeigeText.config(bg="black")
+        AnzeigeText.config(bg=Settings.Textmanager_Hintergrund)
         AnzeigeText.geometry("1920x1080+1920+0")
         AnzeigeText.overrideredirect(True)
         Text_Anzeige_Label = Label(AnzeigeText, font=("Helvetica", 60), fg="white", bg="black", wraplength=1920)
--- Settings.py
+++ Settings.py
@@ -13,14 +13,20 @@ if __name__ == "__main__":
 
 
 def Check_settings():
-    global see_the_text
-    with open(f"C:\\Users\\{Programm_ort}\\Desktop\\Textmanager Daten\\Textmanager Zwischenspeicher\\see_the_text.txt") as see_the_textinfo:
+    global see_the_text, Textmanager_Hintergrund, Textmanager_Textfarbe
+    with open(f"C:\\Users\\{Programm_ort}\\Desktop\\Textmanager Daten\\Textmanager Daten\\see_the_text.txt") as see_the_textinfo:
         see_the_text = see_the_textinfo.read()
-    Neue_Textmanager.Load_Setting()
+    with open(f"C:\\Users\\{Programm_ort}\\Desktop\\Textmanager Daten\\Textmanager Daten\\Hintergrund.txt") as Neue_Texmanager_Hinregrund:
+        Textmanager_Hintergrund = Neue_Texmanager_Hinregrund.read()
+    with open(f"C:\\Users\\{Programm_ort}\\Desktop\\Textmanager Daten\\Textmanager Daten\\Textfarbe.txt") as Neue_Texmanager_Textfarbe:
+        Textmanager_Textfarbe = Neue_Texmanager_Textfarbe.read()
 
 
 def make_settings():
     Settings = Toplevel(Neue_Textmanager.Textmanager)
+    Settings.geometry("600x800")
+    Settings.config(bg=Textmanager_Hintergrund)
+
 
 def Info():
     global Info_manager
@@ -30,9 +36,9 @@ def Info():
         pass
     Info_manager = Toplevel(Neue_Textmanager.Textmanager)
     Info_manager.geometry("800x600")
-    Info_manager.config(bg="black")
+    Info_manager.config(bg=Textmanager_Hintergrund)
     Text_für_Info = "Entwickler/ Uhrheber: Tobias Giebelhaus\nIn gedenken an meinen Geliebten Opa der bis zum Schluss Geistig fit war und sorgen kurz vor dem Tod im Internet war. Er war ein sehr lieber Opa"
-    Info_zum_programm = Label(Info_manager, font=("Halvetica", 15), bg="black", fg="green", text=Text_für_Info, wraplength=800)
+    Info_zum_programm = Label(Info_manager, font=("Halvetica", 15), bg=Textmanager_Hintergrund, fg=Textmanager_Textfarbe, text=Text_für_Info, wraplength=800)
     Info_zum_programm["justify"] = "left"
     Info_zum_programm.place(x=0,y=0)
     Bild_für_opa1 = Image.open(f"C:\\Users\\{Programm_ort}\\Desktop\\Textmanager Daten\\Textmanager Daten\\Sterbe Anzeige Opa.jpg")
