33e0ae8db49fdfa7c3dcec5ea501f0c642bd6d63
TechnikTobias <tobias.giebelhaus@gmx.de>
2023-05-26 21:07:41 +0200

    main

--- Settings.py
+++ Settings.py
@@ -6,54 +6,73 @@ from PIL import Image, ImageTk
 
 Programm_ort = os.getlogin()
 
-if __name__ == "__main__":
-    pass
-
 
 
 def Check_settings():
-    global see_the_text, Textmanager_Hintergrund, Textmanager_Textfarbe, sd
+    global see_the_text, Textmanager_Hintergrund, Textmanager_Textfarbe, open_datei, Vers_kontroll
     with open(f"Textmanager Daten\\Textmanager Daten\\see_the_text.txt", "r", encoding='utf8') as see_the_textinfo:
         see_the_text = see_the_textinfo.read() == "True"
-        print (see_the_text)
+    with open(f"Textmanager Daten\\Textmanager Daten\\open_text.txt", "r", encoding='utf8') as see_the_textinfo:
+        open_datei = see_the_textinfo.read() == "True"
+    with open(f"Textmanager Daten\\Textmanager Daten\\Vers_kontroll.txt", "r", encoding='utf8') as see_the_textinfo:
+        Vers_kontroll = see_the_textinfo.read() == "True"
     with open(f"Textmanager Daten\\Textmanager Daten\\Hintergrund.txt", "r", encoding='utf8') as Neue_Texmanager_Hinregrund:
         Textmanager_Hintergrund = Neue_Texmanager_Hinregrund.read()
     with open(f"Textmanager Daten\\Textmanager Daten\\Textfarbe.txt", "r", encoding='utf8') as Neue_Texmanager_Textfarbe:
         Textmanager_Textfarbe = Neue_Texmanager_Textfarbe.read()
 
-def switch():
-    with open(f"Textmanager Daten\\Textmanager Daten\\see_the_text.txt", "w", encoding='utf8') as see_the_textinfo:
-        see_the_textinfo.write("True")
-    Photo1 = Image.open("on-button.png")
-    Photo = ImageTk.PhotoImage(Photo1.resize((50,50)))
-    Textanzeiger_setting.config(image=Photo, command=switch1)
-    Textanzeiger_setting.m
 
 
-def switch1():
-    with open(f"Textmanager Daten\\Textmanager Daten\\see_the_text.txt", "w", encoding='utf8') as see_the_textinfo:
-        see_the_textinfo.write("False")
-    Photo1 = Image.open("off-button.png")
-    Photo = ImageTk.PhotoImage(Photo1.resize((50,50)))
-    Textanzeiger_setting.config(image=Photo, command=switch)
-    Textanzeiger_setting.m
+
+
+
+
+
+
 
 def make_settings():
     Check_settings()
-    global Textanzeiger_setting
+    global Textanzeiger_setting_class, Datei_open_class, Vers_kontroll_class, Settings
     Settings = Toplevel(Neue_Textmanager.Textmanager)
     Settings.geometry("600x800")
     Settings.config(bg=Textmanager_Hintergrund)
-    if not see_the_text:
+    Textanzeiger_setting_class = Swich_generator(Settings, 10, 10, "Liedtextanzeige", 70, f"Textmanager Daten\\Textmanager Daten\\see_the_text.txt", see_the_text)
+    Datei_open_class = Swich_generator(Settings, 10, 70, "Datei Öffnen", 70, f"Textmanager Daten\\Textmanager Daten\\open_text.txt", open_datei)
+    Vers_kontroll_class = Swich_generator(Settings, 10, 130, "Vers Kontrolle", 70, f"Textmanager Daten\\Textmanager Daten\\Vers_kontroll.txt", Vers_kontroll)
+
+
+class Swich_generator:
+    def __init__(self, Settings_is, x_pos, y_pos, Textnazeige, x_pos_text, Text_datei_save, ob_True):
+        self.Text_datei_save = Text_datei_save
+        self.is_switch = Button(Settings_is)
+        self.switch_text = Label(Settings_is, font=("Helvetica", 12), bg=Textmanager_Hintergrund, fg=Textmanager_Textfarbe, text=Textnazeige)
+        self.switch_text.place(x=x_pos_text, y=y_pos + 12)
+        if not ob_True:
+            self.Photo1 = Image.open("off-button.png")
+            self.Photo = ImageTk.PhotoImage(self.Photo1.resize((50,50)))
+            self.is_switch.config(image=self.Photo, bg=Textmanager_Hintergrund, border=0, command=self.switch_setting_on)
+        else:
+            self.Photo1 = Image.open("on-button.png")
+            self.Photo = ImageTk.PhotoImage(self.Photo1.resize((50,50)))
+            self.is_switch.config(image=self.Photo, bg=Textmanager_Hintergrund, border=0, command=self.switch_setting_off)
+        self.is_switch.place(x=x_pos, y=y_pos)
+    def switch_setting_off(self):
+        with open(f"{self.Text_datei_save}", "w", encoding='utf8') as see_the_textinfo:
+            see_the_textinfo.write("False")
         Photo1 = Image.open("off-button.png")
         Photo = ImageTk.PhotoImage(Photo1.resize((50,50)))
-        Textanzeiger_setting = Button(Settings, image=Photo, bg=Textmanager_Hintergrund, border=0, command=switch)
-    else:
+        self.is_switch.configure(image=Photo, command= self.switch_setting_on)
+        self.is_switch.draw()
+
+    def switch_setting_on(self):
+        with open(f"{self.Text_datei_save}", "w", encoding='utf8') as see_the_textinfo:
+            see_the_textinfo.write("True")
         Photo1 = Image.open("on-button.png")
         Photo = ImageTk.PhotoImage(Photo1.resize((50,50)))
-        Textanzeiger_setting = Button(Settings, image=Photo, bg=Textmanager_Hintergrund, border=0, command=switch1)
-    Textanzeiger_setting.pack()
-    Textanzeiger_setting.m
+        self.is_switch.configure(image=Photo, command=self.switch_setting_off)
+        self.is_switch.draw()
+
+
 
 def Info():
     global Info_manager
@@ -68,7 +87,7 @@ def Info():
     Info_zum_programm = Label(Info_manager, font=("Halvetica", 15), bg=Textmanager_Hintergrund, fg=Textmanager_Textfarbe, text=Text_für_Info, wraplength=800)
     Info_zum_programm["justify"] = "left"
     Info_zum_programm.place(x=0,y=0)
-    Bild_für_opa1 = Image.open(f"C:\\Users\\{Programm_ort}\\Desktop\\Textmanager Daten\\Textmanager Daten\\Sterbe Anzeige Opa.jpg")
+    Bild_für_opa1 = Image.open(f"Textmanager Daten\\Textmanager Daten\\Sterbe Anzeige Opa.jpg")
     Bild_für_opa = ImageTk.PhotoImage(image=Bild_für_opa1.resize((472,341))) 
     Bild_für_opa_Label = Label(Info_manager,image=Bild_für_opa)
     Bild_für_opa_Label.place(x=0,y=100)
