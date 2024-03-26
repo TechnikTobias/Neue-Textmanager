9e6d6af92d9650f4f66757729f62f990a7bac247
TechnikTobias <tobias.giebelhaus@gmx.de>
2024-03-26 18:25:57 +0100

    Laptop

--- ablauf.py
+++ ablauf.py
@@ -9,6 +9,53 @@ def Präsentation_starten():
         alle_inhalt.append(gegerator_lieder(i))
         Position_präsi(1)
     Neue_Textmanager.Textmanager.bind("<Configure>", on_resize)
+    Neue_Textmanager.Textmanager.bind("<Key>", on_key)
+    anfang_ablauf()
+
+def on_key(event):
+        if event.keysym == 'Up':
+            zurueck_lied()
+        elif event.keysym == 'Down':
+            weiter_lied()
+        elif event.keysym == 'Right':
+            weiter_vers()
+        elif event.keysym == 'Left':
+            zurueck_vers()
+
+
+def weiter_vers():
+    ablauf_varables[2] += 1
+    if ablauf_varables[2] >=1 and ablauf_varables [2] < alle_inhalt[ablauf_varables[1]][1][3]:
+        print(alle_inhalt[ablauf_varables[1]][1][3])
+    ablauf_akualisieren()
+
+def weiter_lied():
+    ablauf_varables[1] += 1
+    ablauf_akualisieren()
+
+
+def zurueck_vers():
+    ablauf_varables[2] -= 1
+    ablauf_akualisieren()
+
+def zurueck_lied():
+    ablauf_varables[1] -= 1
+    ablauf_akualisieren()
+
+def anfang_ablauf():
+    global ablauf_varables
+    ablauf_varables = []
+    ablauf_varables.append(alle_inhalt)
+    ablauf_varables.append(0) 
+    ablauf_varables.append(0) 
+
+def ende_ablauf():
+    global ablauf_varables
+    del ablauf_varables
+
+
+def ablauf_akualisieren():
+    print(alle_inhalt[ablauf_varables[1]][1][1])
 
 
 def on_resize(event):
@@ -23,8 +70,6 @@ def gegerator_lieder(input):
     inhalt.append(Lied_start)
     if aktion == " Textwort":
         inhalt.append([aktion, name_lied])
-        Button_Textwrt = Button(Neue_Textmanager.Textmanager, text="Textwort")
-        inhalt.append(Button_Textwrt)
     elif aktion == " Lied":
         inhalt.append([aktion, name_lied, ja[5], ja[3], ja[4]])
         Lable_Kamera = Button(Neue_Textmanager.Textmanager, text="Kamera", border=0)
@@ -43,16 +88,13 @@ def Position_präsi(factor):
         text_size = min( int(i[0].winfo_height()/3*factor), int(i[0].winfo_width()/8*factor))
         i[0].config(font=('Helvetica', text_size))
         if i[1][0] == " Textwort":
-            i[0].place(x= 0, y= pos*i[0].winfo_height()*2+pos*2,relwidth=0.3*factor, relheight=0.05*factor)
-            i[2].place(x= 0, y= pos*(i[0].winfo_height()+1)*2+i[0].winfo_height()+1,relwidth=0.3*factor, relheight=0.05*factor)
-            i[2].config(font=('Helvetica', text_size))
+            i[0].place(x= 0, y= pos*i[0].winfo_height()+pos*2,relwidth=0.4*factor, relheight=0.1*factor)
         elif i[1][0] == " Lied":
             i[0].place(x= 0, y= pos*i[0].winfo_height()*2+pos*2,relwidth=0.3*factor, relheight=0.05*factor)
             i[2].place(x= 0, y= pos*(i[0].winfo_height()+1)*2+i[0].winfo_height()+1,relwidth=0.3*factor, relheight=0.05*factor)
             i[2].config(font=('Helvetica', text_size))
             i[3].place(x= (i[0].winfo_width()+1), y= pos*i[0].winfo_height()*2+pos*2,relwidth=0.4*factor, relheight=0.1*factor)
             i[3].config(font=('Helvetica', text_size))
-            print(i[1])
             song = Neue_Textmanager.get_db_connection("SELECT song_name FROM songs WHERE song_number = ? AND book_name = ?", (str(i[1][3]),str(i[1][2])), True)
             if not i[1][3]:
                 text_einfügen = ""
