a2bc3894e6b1c33d20ccb2ebba03fb06d26a05ef
Tobias <tobias.giebelhaus@gmx.de>
2024-05-31 00:02:24 +0200

    Linux PC

--- Neue_Textmanager.py
+++ Neue_Textmanager.py
@@ -17,8 +17,18 @@ class TextmanagerAPP:
         self.Textmanager.geometry("1040x800")
         self.widget_info = {}
         self.Menu_generator()
-
+        verses = self.db_connection_info_get("SELECT supjekt FROM Einstellungen WHERE name = ?", ("Ablauf",))
+        input_lieder = (str(verses).split("!"))
+        for pos, i in enumerate (input_lieder):
+            self.gegerator_lieder(i, power_range=pos+1)
+        self.Textmanager.bind("<Configure>", self.one_resize)
         self.Textmanager.mainloop()
+
+
+    def one_resize(self,event):
+        Load_settings.Textmanager_größen(self)
+        self.update_widget_positions()
+        self.Textmanager.unbind("<Configure>")
 
 
     def get_window_size(self):
@@ -29,22 +39,21 @@ class TextmanagerAPP:
 
     def register_widget(self,
             name: str, 
-            widget_place, 
             widget: Widget = None, 
             relheight: int = 0.1, 
-            relwidth : int = .11,
+            relwidth : int = 0.11,
             relx: int = None, 
             rely: int = None):
         """Registriert ein Widget und speichert seine Informationen
         Hier werden die Info für die Widget übergeben damit sie an einer zentrallen stelle placiert werden und deren größe angepasst wird."""
         self.widget_info[name] = {
-            "widget_place" : widget_place,
             "widget": widget,
             "relheight": relheight,
             "relwidth": relwidth,
             "relx": relx,
             "rely": rely
         }
+
 
     def get_db_connection(self):
         self.db_filename = "Lieder_Datenbank.db"
@@ -67,58 +76,107 @@ class TextmanagerAPP:
         self.Ausgabe = self.cursor.fetchall()
         self.conn.close()
         return self.Ausgabe[0][0]
+        
     
 
     def Menu_generator(self):
         hintergrund_farbe = self.db_connection_info_get("SELECT supjekt FROM Einstellungen WHERE name = ?", ("hintergrundfarbe",))
         text_farbe = self.db_connection_info_get("SELECT supjekt FROM Einstellungen WHERE name = ?", ("text_farbe",))
-        menu_info_main = ttk.Menubutton(self.Textmanager, text='Info', style='custom.TMenubutton')
-        menu_info = Menu(menu_info_main, bg=hintergrund_farbe, fg= text_farbe, border=0, borderwidth=0, tearoff=False, )
-        menu_kamera_main = ttk.Menubutton(self.Textmanager, text = "Kamera", style='custom.TMenubutton')
-        menu_kamera = Menu(menu_kamera_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
-        menu_liedkontrolle_main = ttk.Menubutton(self.Textmanager, text = "Liedkontrolle", style='custom.TMenubutton')
-        menu_liedkontrolle = Menu(menu_liedkontrolle_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
-        menu_help_main = ttk.Menubutton(self.Textmanager, text = "Hilfe", style='custom.TMenubutton')
-        menu_help = Menu(menu_help_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
-        menu_info.add_radiobutton(label="Einstellungen", command=self.festgröße_bestimmen)
-        menu_info.add_radiobutton(label= "Info", command=Settings.Info)
+        self.menu_info_main = ttk.Menubutton(self.Textmanager, text='Info', style='custom.TMenubutton')
+        menu_info = Menu(self.menu_info_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
+        self.menu_kamera_main = ttk.Menubutton(self.Textmanager, text="Kamera", style='custom.TMenubutton')
+        menu_kamera = Menu(self.menu_kamera_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
+        self.menu_liedkontrolle_main = ttk.Menubutton(self.Textmanager, text="Liedkontrolle", style='custom.TMenubutton')
+        menu_liedkontrolle = Menu(self.menu_liedkontrolle_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
+        self.menu_help_main = ttk.Menubutton(self.Textmanager, text="Hilfe", style='custom.TMenubutton')
+        menu_help = Menu(self.menu_help_main, bg=hintergrund_farbe, fg=text_farbe, border=0, borderwidth=0, tearoff=False)
+        menu_info.add_radiobutton(label="Einstellungen", command=Load_settings.Textmanager_größen(self))
+        menu_info.add_radiobutton(label="Info", command=Settings.Info)
         menu_kamera.add_command(label="Einstellungen", command=Kamera.Settings)
         menu_kamera.add_command(label="Position", command=Kamera.Position)
         menu_liedkontrolle.add_command(label="Einstellungen")
         menu_liedkontrolle.add_command(label="Lied Kontrolieren", command=datenverarbeiten.setup_ui)
         menu_help.add_command(label="Hilfe")
-        menu_info_main['menu'] = menu_info
-        menu_kamera_main['menu'] = menu_kamera
-        menu_help_main["menu"] = menu_help
-        menu_liedkontrolle_main["menu"] = menu_liedkontrolle
-        menu_info_main.pack(side=LEFT, anchor=NW)
-        menu_kamera_main.pack(side=LEFT, anchor=NW)
-        menu_liedkontrolle_main.pack(side=LEFT, anchor=NW)
-        menu_help_main.pack(side=LEFT, anchor=NW)
+        self.menu_info_main['menu'] = menu_info
+        self.menu_kamera_main['menu'] = menu_kamera
+        self.menu_help_main["menu"] = menu_help
+        self.menu_liedkontrolle_main["menu"] = menu_liedkontrolle
+        self.menu_info_main.pack(side=LEFT, anchor=NW)
+        self.menu_kamera_main.pack(side=LEFT, anchor=NW)
+        self.menu_liedkontrolle_main.pack(side=LEFT, anchor=NW)
+        self.menu_help_main.pack(side=LEFT, anchor=NW)
+        
+
+
+    def gegerator_lieder(self, input, power_range):
+        ja = input.split(",")
+        name_lied = ja[0].split(":")
+        aktion = ja[1].split(":")
+        inhalt = []
+        inhalt.append([aktion[1], name_lied[1]])
+        self.Lied_start = ttk.Label(self.Textmanager, text=name_lied[1], style='TLabel')
+        self.register_widget(f"Lied_start{power_range}", self.Lied_start, relheight=0.05, relwidth=0.15, rely=0.1*power_range-0.05, relx=0.0)
+        if aktion[1] == " Textwort":
+            self.Button_Textwort = ttk.Button(self.Textmanager, text="Textwort", style='TButton')
+            self.register_widget(f"Button_textwort{power_range}", self.Button_Textwort, relheight=0.1, relwidth=0.15, rely=0.1*power_range-0.05, relx=0.15)
+        elif aktion[1] == " Lied":
+            self.Lable_Kamera = ttk.Button(self.Textmanager, text="Kamera", style='TButton')
+            self.register_widget(f"Lable_Kamera{power_range}", self.Lable_Kamera, relheight=0.1, relwidth=0.15, rely=0.1*power_range-0.05, relx=0.15)
+            befehle = ["Kamera", "Textwort", "Lied"]
+            clicked = StringVar()
+            clicked.set(befehle[0])
+            opt = ttk.OptionMenu(self.Textmanager, clicked, *befehle, style='custom.TMenubutton')
+            inhalt.append(clicked)
+            inhalt.append(opt)
+            eingabe_Lied = ttk.Entry(self.Textmanager, style='TEntry')
+            eingabe_Lied.bind("<KeyRelease>", eingabe_änderung)
+            inhalt.append(eingabe_Lied)
+            eingabe_Vers = ttk.Entry(self.Textmanager, style='TEntry')
+            eingabe_Vers.bind("<KeyRelease>", eingabe_änderung)
+            inhalt.append(eingabe_Vers)
+            befehle_buch = ["Gesangbuch", "Chorbuch", "Jugendliederbuch"]
+            clicked_buch = StringVar()
+            clicked_buch.set(befehle_buch[0])
+            opt_buch = ttk.OptionMenu(self.Textmanager, clicked_buch, *befehle_buch, command=eingabe_änderung)
+            inhalt.append(clicked_buch)
+            inhalt.append(opt_buch)
+            Tex_lied_lable = ttk.Label(self.Textmanager)
+            inhalt.append(Tex_lied_lable)
+        elif aktion[1] == " Kamera":
+            lied_weiter = ttk.Button(self.Textmanager, text= "servus", style='TButton')
+            inhalt.append(lied_weiter)
+            befehle = ["Kamera", "Textwort", "Lied"]
+            clicked = StringVar()
+            clicked.set(befehle[0])
+            inhalt.append(clicked)
+            opt = ttk.OptionMenu(self.Textmanager, clicked, *befehle)
+            opt.config(style='custom.TMenubutton')
+            inhalt.append(opt)
+        return inhalt
+
+
+
+
 
     def update_widget_positions(self):
         """Aktualisiert die Positionen und Größen aller Widgets basierend auf dem Skalierungsfaktor"""
-        factor = int(self.db_connection_info_get("SELECT supjekt FROM Einstellungen WHERE name = ?", ("scalierung",))[0])/100
-        widgets_to_remove = []
-        print(self.widget_info)
-        for name, info in self.widget_info.items():
+        self.factor = int(self.db_connection_info_get("SELECT supjekt FROM Einstellungen WHERE name = ?", ("scalierung",)))/100
+        self.widgets_to_remove = []
+        for self.name, self.info in self.widget_info.items():
             try:
-                print("erledigt")
-                widget_place = info["widget_place"]
-                widget = info["widget"]
-                relheight = info["relheight"] * factor
-                relwidth = info["relwidth"] * factor
-                relx = info["relx"] * factor
-                rely = info["rely"] * factor
-                widget.place( relwidth=relwidth, relheight=relheight, relx=relx, rely=rely)
+                self.widget = self.info["widget"]
+                self.relheight = self.info["relheight"] * self.factor
+                self.relwidth = self.info["relwidth"] * self.factor
+                self.relx = self.info["relx"] * self.factor
+                self.rely = self.info["rely"] * self.factor
+                self.widget.place(relwidth=self.relwidth, relheight=self.relheight, relx=self.relx, rely=self.rely)
             except:
-                widgets_to_remove.append(name)
-                print("fehler")
+                self.widgets_to_remove.append(self.name)
 
         # Entferne die fehlerhaften Widgets aus widget_info
-        for name in widgets_to_remove:
-            del self.widget_info[name]
-            print(f"Widget {name} aus widget_info entfernt")
+        for self.name in self.widgets_to_remove:
+            del self.widget_info[self.name]
+            print(f"Widget {self.name} aus widget_info entfernt")
 
 
 
@@ -156,8 +214,9 @@ def get_db_connection(input_db, input_db_variabel, get_output = True):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute(input_db, input_db_variabel)
-    conn.commit()
+    Ausgabe = cursor.fetchall()
     conn.close()
+    return Ausgabe[0]
 
 
 
@@ -384,3 +443,4 @@ def posistion():
             i[2].place(x=fenster_width-i[2].winfo_width()-15, y=menu_info_main.winfo_height()+1+i[1].winfo_height()+10, relwidth=0.12*factor, relheight=0.07*factor)
             i[3].place(x=fenster_width-i[1].winfo_width()-15, y=menu_info_main.winfo_height()+1+i[1].winfo_height()+i[2].winfo_height()+10, relwidth=0.12*factor, relheight=0.07*factor)
             i[4].place(x=fenster_width-i[1].winfo_width()-15, y=menu_info_main.winfo_height()+1+i[1].winfo_height()+i[2].winfo_height()+i[3].winfo_height()+10, relwidth=0.12*factor, relheight=0.07*factor)
+
--- Start.py
+++ Start.py
@@ -1,3 +1,6 @@
 import Neue_Textmanager
 
-App_textmanager_main =Neue_Textmanager.TextmanagerAPP()
+#App_textmanager_main = Neue_Textmanager.TextmanagerAPP()
+
+Neue_Textmanager.Start()
+Neue_Textmanager.Textmanager.mainloop()
